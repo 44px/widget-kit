f773e2e14dee5d579693cfd6375bcc82ceaef6c3
Alexander Shepelin <brightcor@gmail.com>
2018-12-03 08:51:31 +0300

    Allow to set right/bottom values in PositionFixed plugin

--- packages/container/src/plugins/position-fixed.test.ts
+++ packages/container/src/plugins/position-fixed.test.ts
@@ -2,28 +2,50 @@ import { getContainerAndWidget } from './test-utils';
 import { initPositionFixedPlugin, getPosition, setPosition } from './position-fixed';
 
 describe('PositionFixed plugin', () => {
+  it('keeps default values', () => {
+    const [container, _] = getContainerAndWidget();
+    initPositionFixedPlugin(container, {});
+    expect(container.iframe.style.position).toBe('fixed');
+    expect(container.iframe.style.top).toBe('');
+    expect(container.iframe.style.left).toBe('');
+    expect(container.iframe.style.right).toBe('');
+    expect(container.iframe.style.bottom).toBe('');
+  });
+
   it('sets initial values', () => {
     const [container, _] = getContainerAndWidget();
     initPositionFixedPlugin(container, {
-      initialTop: '10px',
-      initialLeft: '20px',
+      initialPosition: {
+        top: '10px',
+        left: '20px',
+        right: '30px',
+        bottom: '40px',
+      },
     });
     expect(container.iframe.style.position).toBe('fixed');
     expect(container.iframe.style.top).toBe('10px');
     expect(container.iframe.style.left).toBe('20px');
+    expect(container.iframe.style.right).toBe('30px');
+    expect(container.iframe.style.bottom).toBe('40px');
   });
 
   it('handles getPosition request', (done) => {
     const [container, widget] = getContainerAndWidget();
     initPositionFixedPlugin(container, {
-      initialTop: '10px',
-      initialLeft: '20px',
+      initialPosition: {
+        top: '10px',
+        left: '20px',
+        right: '30px',
+        bottom: '40px',
+      },
     });
     widget.send(getPosition(), (response) => {
       // JSDOM implementation of getBoundingClientRect returns zeros for all values
       // So check here just shape of response but not values
       expect(response.payload.top).toBeDefined();
       expect(response.payload.left).toBeDefined();
+      expect(response.payload.right).toBeDefined();
+      expect(response.payload.bottom).toBeDefined();
       done();
     });
   });
@@ -31,13 +53,26 @@ describe('PositionFixed plugin', () => {
   it('handles setPosition request', (done) => {
     const [container, widget] = getContainerAndWidget();
     initPositionFixedPlugin(container, {
-      initialTop: '10px',
-      initialLeft: '20px',
+      initialPosition: {
+        top: '10px',
+        left: '20px',
+        right: '30px',
+        bottom: '40px',
+      },
     });
-    widget.send(setPosition('50%', '40%'));
+    widget.send(
+      setPosition({
+        top: '50%',
+        left: '40%',
+        right: '30%',
+        bottom: '20%',
+      }),
+    );
     setTimeout(() => {
       expect(container.iframe.style.top).toBe('50%');
       expect(container.iframe.style.left).toBe('40%');
+      expect(container.iframe.style.right).toBe('30%');
+      expect(container.iframe.style.bottom).toBe('20%');
       done();
     });
   });
@@ -45,27 +80,17 @@ describe('PositionFixed plugin', () => {
   it('handles setPosition request: only top', (done) => {
     const [container, widget] = getContainerAndWidget();
     initPositionFixedPlugin(container, {
-      initialTop: '10px',
-      initialLeft: '20px',
+      initialPosition: {
+        top: '10px',
+        left: '20px',
+      },
     });
-    widget.send(setPosition('100px', null));
+    widget.send(setPosition({ top: '100px' }));
     setTimeout(() => {
       expect(container.iframe.style.top).toBe('100px');
       expect(container.iframe.style.left).toBe('20px');
-      done();
-    });
-  });
-
-  it('handles setPosition request: only left', (done) => {
-    const [container, widget] = getContainerAndWidget();
-    initPositionFixedPlugin(container, {
-      initialTop: '10px',
-      initialLeft: '20px',
-    });
-    widget.send(setPosition(null, '100px'));
-    setTimeout(() => {
-      expect(container.iframe.style.top).toBe('10px');
-      expect(container.iframe.style.left).toBe('100px');
+      expect(container.iframe.style.right).toBe('');
+      expect(container.iframe.style.bottom).toBe('');
       done();
     });
   });
