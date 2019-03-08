1002ae968d547ea14b01fba20101effd13600246
Alexander Shepelin <Brightcor@gmail.com>
2019-03-08 22:51:22 +0300

    Re-export less from rpc package

--- packages/container/src/index.ts
+++ packages/container/src/index.ts
@@ -1,3 +1,11 @@
-export * from '@widget-kit/rpc';
+export {
+  Request,
+  Response,
+  ResponseProducerFn,
+  ResponseHandlerFn,
+  HandlerFn,
+  Connection,
+  createRequest,
+} from '@widget-kit/rpc';
 export { Config, Container, createContainer } from './container';
 export { Widget, initWidget } from './widget';
